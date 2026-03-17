a75890cde1b538d9764d4a30c2e27310a33b66bb
Merk42 <mark_c@markecurtis.com>
2026-03-17 10:34:08 -0400

    allrecipes API

--- src/app/api/browserecipes.php
+++ src/app/api/browserecipes.php
@@ -6,17 +6,9 @@ $request = $data->request ?? null;
 
 $sql = "SELECT * FROM recipes";
 $result = $mysqli->query($sql);
-/*
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-*/
+
   $response = array();
-  while($row = mysqli_fetch_assoc($sql)){
+  while($row = $result->fetch_assoc()) {
     $response[] = $row;
   }
 
@@ -25,6 +17,4 @@ if ($result->num_rows > 0) {
 
 // Close the result set
 $result->close();
-?>
-
 ?>
